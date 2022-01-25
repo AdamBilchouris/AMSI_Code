c7cbc74e20aad041c13f8d46fbdf2a13ea4b7bc5
Adam Bilchouris <thesteamyocelot@gmail.com>
2022-01-25 18:56:13 +1100

    Broke Looped.R

--- Looped.R
+++ Looped.R
@@ -22,15 +22,21 @@ data3 <- data3[order(data3$index), ]
 
 data4 <- merge(x=data3, y=crimeSum, by='suburb', all.x=T)
 
+data4 <- data4[data4$suburb != 'Albert Park', ]
+data4 <- data4[order(data4$index), ]
+
 # No lat/lng for both
 # No crime
 data5 <- data4[, c(2, 4:8, 13:16, 19:21)]
+dataStd <- data4[, c(2, 4:8, 13:16, 19:21)] %>% mutate_all(~(scale(.) %>% as.vector))
 
 #========= For loop for verification =======
 library(dplyr)
 library(stringr)
 
-iters <- 5
+regressNormalRMSEVec <- c()
+
+iters <- 1
 regressRMSEVec <- c()
 histRMSEVec <- c()
 hybridRMSEVec <- c()
@@ -48,7 +54,7 @@ for(i in 1:nrow(historical2)) {
   suburb <- historical2[historical2$suburb == suburbName, ]
   suburbName <- str_to_title(historical2[i, 'suburb'])
   # Get all suburbs in the data which match this name and adjust their prices.
-  dataSuburb <- dataAdj[dataAdj$suburb == suburbName, ]
+  dataSuburb <- data4[data4$suburb == suburbName, ]
   
   # Skip over the entry if the suburb doesn't exist in the dataset.
   if(nrow(dataSuburb) == 0) { next }
@@ -56,53 +62,81 @@ for(i in 1:nrow(historical2)) {
   # For each entry in the subset of the data, increase the price by some inflation factor.
   for(j in 1:nrow(dataSuburb)) {
     rowInt <- rownames(dataSuburb[j, ])
-    year <- as.numeric(format(dataSuburb[rowInt, 'dateObj'], '%Y'))
-    dataSuburb[rowInt, 'year'] <- year
+    year <- as.numeric(format(dataSuburb[j, 'dateObj'], '%Y'))
+    dataSuburb[j, 'year'] <- year
     
     # Adjust by sale data of house.
     if(is.na(year)) { next }
     if(year == 2021) {
-      monthNum <- as.numeric(format(dataSuburb[rowInt, 'dateObj'], '%m'))
+      monthNum <- as.numeric(format(dataSuburb[j, 'dateObj'], '%m'))
       if(monthNum >= 10) { next }
       monthNum <- str_pad(monthNum, 2, 'left', '0')
       testColumn <- paste('2021-10/2021-', as.character(monthNum), sep='')
       ratio <- suburb[1, testColumn]
-      dataSuburb[rowInt, 'price'] <- ratio*dataSuburb[rowInt, 'price']
+      dataSuburb[j, 'price'] <- ratio*dataSuburb[j, 'price']
     }
     else if(year >= 2012) {
       testColumn <- paste('2021', year, sep='/')
       ratio <- suburb[1, testColumn]
-      dataSuburb[rowInt, 'price'] <- ratio*dataSuburb[rowInt, 'price']
+      if(rowInt  == '7742') {
+        print(dataSuburb[j, 'price'])
+      }
+      dataSuburb[j, 'price'] <- ratio*dataSuburb[j, 'price']
     }
     else {
       testColumn <- paste('2021', '2012', sep='/')
       ratio <- suburb[1, testColumn]
-      dataSuburb[rowInt, 'price'] <- ratio*dataSuburb[rowInt, 'price']
+      dataSuburb[j, 'price'] <- ratio*dataSuburb[j, 'price']
     }
   }
+
+  indicesSuburb <- rownames(dataSuburb) 
+  dataAdj[indicesSuburb, 'price'] <- dataSuburb[indicesSuburb, 'price']
   
   #tempLm <- lm(price ~ dateObj, data=dataSuburb)
-  #mean2021 <- mean(dataSuburb[dataSuburb$year == 2021, 'price'])
+  #print(suburbName)
+  #print(summary(tempLm))
+  #mean2021 <- mean(dataSuburb[dataSuburb$year == 2021, ]$price)
+  #
+  #indicesSuburb <- rownames(dataSuburb) 
+  #if(is.na(mean2021)) {
+  #  mean2021_20 <- mean(filter(dataSuburb, between(year, 2020, 2021))$price)
+  #
+  #  if(is.na(mean2021_20)) {
+  #    dataAdj[indicesSuburb, 'price'] <- dataSuburb[indicesSuburb, 'price']
+  #    next
+  #  }
+  #  dataSuburb$price2 <- dataSuburb$price - as.numeric(tempLm$coefficients[2])*as.numeric(dataSuburb$dateObj) + mean2021_20
+  #  dataAdj[indicesSuburb, 'price'] <- dataSuburb[indicesSuburb, 'price2']
+  #  next
+  #}
+  #
   #dataSuburb$price2 <- dataSuburb$price - as.numeric(tempLm$coefficients[2])*as.numeric(dataSuburb$dateObj) + mean2021
-  indicesSuburb <- rownames(dataSuburb) 
   #dataAdj[indicesSuburb, 'price'] <- dataSuburb[indicesSuburb, 'price2']
-  dataAdj[indicesSuburb, 'price'] <- dataSuburb[indicesSuburb, 'price']
 }
 
 for(it in 1:iters) {
   keepIndexNew <- c()
-  for(i in 1:nrow(data)) {
+  for(i in 1:nrow(data4)) {
     # Check if the sale year is in 2021 and check between June and September 
-    yearKeep <- str_split(data[i, 'date'], '-')[[1]][3] == '2021'
-    month <- match(str_split(data[i, 'date'], '-')[[1]][2], month.abb)
+    #yearKeep <- str_split(data[i, 'date'], '-')[[1]][3] == '2021'
+    rowInt <- rownames(data4[i, ])
+    year <- as.numeric(format(data[rowInt, 'dateObj'], '%Y'))
+    yearKeep <- year == 2021
+    month <- as.numeric(format(data[rowInt,'dateObj'], '%m')) #match(str_split(data[i, 'date'], '-')[[1]][2], month.abb)
     monthKeep <- (month <= 9 && month >= 1)
     histCount <- str_count(data[i, 'salesHistory'], '-')
+    
     if(yearKeep == F || monthKeep == F) {
       next
     }
     
+    if(year != 2021) {
+      print(paste(year, month, paste='-'))
+    }
+    
     histAbove <- 0
-    histPrices <- str_split(data[i, 'salesHistory'], '-')
+    histPrices <- str_split(data[rowInt, 'salesHistory'], '-')
     for(j in 1:length(histPrices[[1]])) {
       info <- str_split(histPrices[[1]][j], '/')[[1]]
       saleMonth <- info[1]
@@ -116,7 +150,7 @@ for(it in 1:iters) {
         histAbove <- histAbove - 1
       }
       if(salePrice  < 5000) {
-        histAbove <- histAbove - 1
+        histAbove <- histAbove - 2
       }
       else {
         histAbove <- histAbove + 1
@@ -136,13 +170,30 @@ for(it in 1:iters) {
   # Test is 50% of the most recent sales.
   # Train is 3000 houses from the remaining recent sales + older sales.
   testSamples <- sample(keepIndexNew, 0.5*length(keepIndexNew))
-  test <- data5[testSamples, ]
-  #test <- dataAdj2[testSamples, ]
-  trainSamplesVec <- union(setdiff(keepIndexNew, testSamples), setdiff(rownames(data5), keepIndexNew))
+  
+  #test <- data5[as.character(testSamples), ]
+  #test <- dataAdj2[as.character(testSamples), ]
+  #test <- dataStd[as.character(testSamples), ]
+  
+  test <- data5[rownames(data5) %in% testSamples, ]
+  #test <- dataAdj2[rownames(dataAdj2) %in% testSamples, ]
+  #test <- dataStd[rownames(dataStd) %in% testSamples, ]
+  
+  
+  trainSamplesVec <- union(setdiff(keepIndexNew, testSamples), setdiff(as.numeric(rownames(data5)), keepIndexNew))
   #trainSamplesVec <- union(setdiff(keepIndexNew, testSamples), setdiff(rownames(dataAdj2), keepIndexNew))
+  #trainSamplesVec <- union(setdiff(keepIndexNew, testSamples), setdiff(rownames(dataStd), keepIndexNew))
+  
   trainSamples <- sample(trainSamplesVec, 3000)
-  train <- data5[trainSamples, ]
-  #train <- dataAdj2[trainSamples, ]
+  
+  #train <- data5[as.character(trainSamples), ]
+  #train <- dataAdj2[as.character(trainSamples), ]
+  #train <- dataStd[as.character(trainSamples), ]
+  
+  train <- data5[rownames(data5) %in% trainSamples, ]
+  #train <- dataAdj2[rownames(dataAdj2) %in% trainSamples, ]
+  #train <- dataStd[rownames(dataStd) %in% trainSamples, ]
+  
   
   y <- 'price'
   #formulaNew <-  paste0(".*. + I(", names(dataAdj2)[names(dataAdj2)!=y], "^2)+", collapse="") %>%
@@ -164,7 +215,8 @@ for(it in 1:iters) {
     X <- cbind(1, x[, inc])
     mod <- lm.fit(X, y)
     class(mod) <- 'lm'
-    -AIC(mod)
+    #-AIC(mod)
+    -extractAIC(mod)[2]
   }
   
   library(GA)
@@ -181,7 +233,7 @@ for(it in 1:iters) {
   newFormulaNew <- as.formula(formulaStrNew)
   modSelectedNew <- lm(newFormulaNew, data=train)
   
-  predictSelected <- data.frame(pred=predict(modSelectedNew, newdata=test), actual=test$price, 
+  predictSelected <- data.frame(pred=predict(modSelectedNew, newdata=test), predNormal=predict(modNormalNew, newdata=test), actual=test$price, 
                               index=as.numeric(rownames(test)), row.names=rownames(test))
 
   # Get the historical prices
@@ -275,12 +327,17 @@ for(it in 1:iters) {
     for(j in 1:length(histPrices)) {
       histPricesIndv <- c(histPricesIndv, as.numeric(histPrices[j]))
     }
+    #joinedPreds[i, 'pred'] <- joinedPreds[i, 'pred']*sd(data5[, 1]) + mean(data5[, 1])
     predPrice <- joinedPreds[i, 'pred']
+    
     joinedPreds[i, 'hybrid'] <- mean(c(histPricesIndv, predPrice))
   }
   
   row.names(joinedPreds) <- joinedPreds$index
   joinedPreds <- joinedPreds[ordered(joinedPreds$index), ]
+  
+  regressNormalMSE <- mean((predictSelected$predNormal - predictSelected$actual)^2)
+  regressNormalRMSE <- sqrt(regressNormalMSE)
   
   # Regression MSE, RMSE
   regressMSE <- mean((joinedPreds$pred - joinedPreds$actual)^2)
@@ -581,7 +638,7 @@ for(it in 1:iters) {
           }
           else if(tempSalesYear[k] %in% c(2012, 2013) && saleYear %in% c(2012, 2013)) {
             adjSalePrice <- (salePrice*as.numeric(ratio))
-            salePrice <- mean(c(tempSalesPrice[k], ajdSalePrice))
+            salePrice <- mean(c(tempSalesPrice[k], adjSalePrice))
             histPrices[k, 'price'] <- salePrice
             histPrices[k, 'year'] <- tempSalesYear[k] 
           }
@@ -674,6 +731,8 @@ for(it in 1:iters) {
     s <- paste0('w', i)
     weights[it, s] <- ww[i]
   }
+  
+  regressNormalRMSEVec <- c(regressNormalRMSEVec, regressNormalRMSE)
 }
 
 avgRegressRMSE <- mean(regressRMSEVec)
@@ -683,9 +742,18 @@ avgFancyRMSE <- mean(fancyRMSEVec)
 avgWeights <- data.frame(w1=mean(weights[, 'w1']), w2=mean(weights[, 'w2']),
                          w3=mean(weights[, 'w3']), w4=mean(weights[, 'w4']),
                          w5=mean(weights[, 'w5']), w6=mean(weights[, 'w6']))
+
+
+avgRegressNormalRMSE <- mean(regressNormalRMSEVec)
+avgRegressNormalRMSE
+
 avgRegressRMSE
 avgHistRMSE
 avgHybridRMSE
 avgFancyRMSE
 avgWeights
+
+# Step AIC (MASS package)
+# Book: 1491952962 Practical Statistics for Data Scientists; 50 Essential Concepts [Bruce & Bruce 2017-05-28] {9601C44B}
+# Page: 225
 
